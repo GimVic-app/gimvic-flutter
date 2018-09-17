b9add9dca05196436110d2024d1d5789c5409110
matejm <matejmarinko123@gmail.com>
2018-09-17 21:45:13 +0200

    show original lesson on tap

--- lib/lesson.dart
+++ lib/lesson.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 const TextStyle numberTextStyle =
-    TextStyle(fontSize: 22.0, fontWeight: FontWeight.w300);
+    TextStyle(fontSize: 20.0, fontWeight: FontWeight.w300);
 
-const TextStyle lessonTextStyle = TextStyle(fontSize: 22.0, fontWeight: FontWeight.w500);
+const TextStyle lessonTextStyle =
+    TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500);
 
-const TextStyle classroomTextStyle = TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300);
+const TextStyle classroomTextStyle =
+    TextStyle(fontSize: 16.0, fontWeight: FontWeight.w300);
 
 class Lesson extends StatelessWidget {
   final Map<String, dynamic> data;
@@ -17,7 +19,7 @@ class Lesson extends StatelessWidget {
     if (data['prosta_ura']) {
       return getFreeTimeCard(Theme.of(context).disabledColor);
     } else if (data['je_nadomescanje']) {
-      return getSupLessonCard(Theme.of(context).accentColor);
+      return getSupLessionCard(Theme.of(context).accentColor);
     } else {
       return getLessonCard();
     }
@@ -27,7 +29,7 @@ class Lesson extends StatelessWidget {
     return Card(
         color: color,
         child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+          padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
           child: Row(
             children: <Widget>[
               Text(data['stevilka'].toString(), style: numberTextStyle)
@@ -39,35 +41,137 @@ class Lesson extends StatelessWidget {
   Card getLessonCard() {
     return Card(
         child: Padding(
-      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       child: Row(
         children: <Widget>[
           Text(data['stevilka'].toString(), style: numberTextStyle),
           Expanded(
               child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 12.0),
-              child: Text(data['predmet'], style: lessonTextStyle))),
+                  padding: EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Text(data['predmet'], style: lessonTextStyle))),
           Text(data['ucilnica'].toString(), style: classroomTextStyle),
         ],
       ),
     ));
   }
 
-  Card getSupLessonCard(Color color) {
+  Card getSupLessionCard(Color color) {
     return Card(
       color: color,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-          child: Row(
-            children: <Widget>[
-              Text(data['stevilka'].toString(), style: numberTextStyle),
-              Expanded(
-                  child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 12.0),
-                      child: Text(data['predmet'], style: lessonTextStyle))),
-              Text(data['ucilnica'].toString(), style: classroomTextStyle),
-            ],
+      child: _SupLessonCard(data),
+    );
+  }
+}
+
+class _SupLessonCard extends StatefulWidget {
+  final Map<String, dynamic> data;
+
+  _SupLessonCard(this.data);
+
+  @override
+  _SupLessonCardState createState() => _SupLessonCardState();
+}
+
+class _SupLessonCardState extends State<_SupLessonCard>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  CurvedAnimation _easeInAnimation;
+
+  bool expanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: Duration(milliseconds: 200), vsync: this);
+    _easeInAnimation =
+        CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    setState(() {
+      expanded = !expanded;
+      if (expanded) {
+        _controller.forward();
+      } else {
+        _controller.reverse().then<void>((Null value) {
+          setState(() {});
+        });
+      }
+    });
+  }
+
+  Widget _buildChildren(BuildContext context, Widget child) {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          InkWell(
+            onTap: _handleTap,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Row(
+                children: <Widget>[
+                  Text(widget.data['stevilka'].toString(),
+                      style: numberTextStyle),
+                  Expanded(
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 12.0),
+                          child: Text(widget.data['predmet'],
+                              style: lessonTextStyle))),
+                  Text(widget.data['ucilnica'].toString(),
+                      style: classroomTextStyle),
+                ],
+              ),
+            ),
           ),
-        ));
+          ClipRect(
+            child: Align(
+              heightFactor: _easeInAnimation.value,
+              child: child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget getAdditionalDetails() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Row(children: <Widget>[
+        Text(
+          'Prvotno:',
+          style: TextStyle(fontSize: 16.0),
+        ),
+        Expanded(
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  widget.data['original']['predmet'],
+                  style: lessonTextStyle,
+                ))),
+        Text(
+          widget.data['original']['ucilnica'],
+          style: classroomTextStyle,
+        )
+      ]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bool closed = !expanded && _controller.isDismissed;
+    return AnimatedBuilder(
+      animation: _controller.view,
+      builder: _buildChildren,
+      child: closed ? null : getAdditionalDetails(),
+    );
   }
 }
